eba7d858341247dc6703bb2aa0d634fd45f55217
z1522983846 <z1522983846@gmail.com>
2026-05-27 18:01:49 +0800

    移除 webdriver_manager 依赖
    Removed unused imports for cleaner code.

--- auto_click.py
+++ auto_click.py
@@ -1,11 +1,8 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.action_chains import ActionChains
-from webdriver_manager.chrome import ChromeDriverManager
 import time
 import logging
 import os
